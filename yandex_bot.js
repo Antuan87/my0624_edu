821f168810924387898b53460f340eda3ce37272
Antuan87 <67383804+Antuan87@users.noreply.github.com>
2020-07-15 06:22:46 +0300

    Update yandex_bot.js

--- yandex_bot.js
+++ yandex_bot.js
@@ -5,16 +5,15 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://yandex.ru/*
+// @match        https://crushdrummers.ru/*
 // @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
 // @grant        none
 // ==/UserScript==
 
-let keywords = ["Гобой","Как звучит флейта","Тромбон","Что такое валторна","Фагот","Скрипка","Виолончель"];
-let keyword = keywords[getRandom(0,keywords.length)];
 let button = document.getElementsByClassName("button__text")[1];
 let nextPage = document.getElementsByClassName(' pager__item_kind_next')[0];
 let links = document.links;
-
+let site = location.host;
 function getRandom(min,max){
 	return Math.floor(Math.random()*(max-min)+min);
 }
@@ -31,15 +30,32 @@ function writeKeyword(word){
 	},600);
 }
 
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
 if(button != undefined){
+    let sites = {
+        "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai":["Гобой","Как звучит флейта","Тромбон","Что такое валторна","Фагот","Скрипка","Виолончель"],
+        "crushdrummers.ru":["Шоу барабанщиков","Барабанное шоу","Заказать выступление барабанщиков","Crush барабанщики"]
+    };
+    let site = Object.keys(sites)[getRandom(0,Object.keys(sites).length)];
+    let keywords = sites[site];
+    let keyword = keywords[getRandom(0,keywords.length)];
+    document.cookie = "site="+site;
     writeKeyword(keyword);
 }else if(location.hostname =="yandex.ru" ){
     let flag = true;
+    let site = getCookie("site");
     for(let i=0;i<links.length;i++){
-        if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") !=-1) {
+        if(links[i].href.indexOf(site) !=-1) {
+            let link = links[i];
             links[i].target="_self";
             flag = false;
-            links[i].click();
+            setTimeout(()=>{link.click()},getRandom(1000,3000));
             break;
         }
     }
@@ -54,7 +70,7 @@ if(button != undefined){
         if (getRandom(0,101)>=70) location.href = "https://yandex.ru/";
         else{
             let index = getRandom(0,links.length);
-            if (links[index].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") !=-1)
+            if (links[index].href.indexOf(site) !=-1)
                 links[index].click();
         }
     },getRandom(3000,6000));
